3f10bf0d3f891283cd3f6df9ddc46f4a8b39f6ff
MysticalSkeptic <115025255+MysticalSkeptic@users.noreply.github.com>
2023-04-01 18:48:18 -0500

    did the old push
    amplify push and made the graphql api real. As real as anything made of electronic 1 and 0 patterns can be...
    made in javascript with path src\graphql\**\*.js
    generated all possible mutations and subscriptions
    maximum statement depth 2

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,9 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "servlab7graphql": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
