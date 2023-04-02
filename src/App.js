583d75b2f1ac4432223ce07c04b27d6a0f9b6552
MysticalSkeptic <115025255+MysticalSkeptic@users.noreply.github.com>
2023-04-01 21:17:35 -0500

    added imports
    App.js needed some things.
    Clipped a comment from index.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useReducer } from 'react';
+import { API } from 'aws-amplify';
+import { List } from 'antd';
+import 'antd/dist/antd.css';
+import { listNotes } from './graphql/queries';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello, this works for now</h1>
     </div>
   );
 }
