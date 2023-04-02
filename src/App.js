0468b589cf69e6eded1a1335b773c3777e6ea496
MysticalSkeptic <115025255+MysticalSkeptic@users.noreply.github.com>
2023-04-02 15:20:24 -0500

    Update App.js
    clearing my notes for the next iteration while everything still works
    next we add the subscription

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ const initialState = {
   error: false,
   form: { name: '', description: '' }
 };
-// setting up reducer with only cases to set not array or error
+
 const reducer = (state, action) => {
   switch(action.type) {
     case 'SET_NOTES':
@@ -37,9 +37,9 @@ const reducer = (state, action) => {
 };
 
 const App = () => {
-  // copy from book... i think this is the right spot...
+  
   const [state, dispatch] = useReducer(reducer, initialState);
-  // add fetch notes function
+  
   const fetchNotes = async() => {
     try {
       const notesData = await API.graphql({
@@ -51,7 +51,7 @@ const App = () => {
       dispatch({ type: 'ERROR' });
     }
   };
-  // put create note here as in demo
+  
   const createNote = async() => {
 
     const { form } = state;
@@ -73,15 +73,11 @@ const App = () => {
     };
     
   };
-  // I want the update function before the delete function...
-  // this finds the index of the note to change, reverses the complete property,
-  // updates the array in the local state and passes it to the API
+  
   const updateNote = async(note) => {
-    // I remembered to use async!
     const index = state.notes.findIndex(n => n.id === note.id);
     const notes = [...state.notes];
     notes[index].complete = !note.complete;
-    // that d I left off bit me on adding notes...
     dispatch({ type: 'SET_NOTES', notes })
     try {
       await API.graphql({
@@ -118,18 +114,18 @@ const App = () => {
     name: e.target.name, 
     value: e.target.value
   });
-  // invoke the fetch notes
+  
   useEffect(() => {
     fetchNotes();
   }, []);
-  // and now to put in styles?
+  
   const styles = {
     container: {padding: 20},
     input: {marginBottom: 10},
     item: { textAlign: 'left'},
     p: { color: '#1890ff' }
   };
-  // add call to update function and react fragments to contain all that p
+  
   const renderItem = (item) => (
     <List.Item 
       style={styles.item}
